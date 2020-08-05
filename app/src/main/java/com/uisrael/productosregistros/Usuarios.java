baf8001e9547515dab84bd70fbaecbc13c5e1c2c
DiegoPillajo1989 <dps700@hotmail.com>
2020-08-05 05:21:59 -0500

    Creacion del repositorio

---
+++ app/src/main/java/com/uisrael/productosregistros/Usuarios.java
@@ -0,0 +1,80 @@
+package com.uisrael.productosregistros;
+import java.util.Date;
+
+public class Usuarios {
+    private String id;
+    private String ci;
+    private String nombre;
+    private String apellido;
+    private String provincia;
+    private String ciudad;
+    private Date fechaNacimiento;
+    private String telefono;
+
+    public Usuarios() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCi() {
+        return ci;
+    }
+
+    public void setCi(String ci) {
+        this.ci = ci;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
